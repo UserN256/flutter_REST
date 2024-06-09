4bdc5feaabd08ac2a3d95aa23ff9e6ccdfad2903
UserN256 <MoyAkkDlyaCoding@gmail.com>
2024-06-09 18:27:13 +0300

    Added pages that should exist

---
+++ lib/pages/meals_page.dart
@@ -0,0 +1,134 @@
+// ignore_for_file: must_be_immutable, prefer_const_constructors, non_constant_identifier_names
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+//import 'package:http/http.dart' as http;
+import 'package:http_auth/http_auth.dart';
+import 'package:rest_api/model/user.dart';
+
+class MealsPage extends StatefulWidget {
+  MealsPage({super.key});
+
+  @override
+  State<MealsPage> createState() => _MealsPageState();
+}
+
+class _MealsPageState extends State<MealsPage> {
+  List<User> users = [];
+
+  void CheckBoxChanged(bool? value, int index) {
+    setState(() {
+      users[index].isEnabled = !users[index].isEnabled;
+    });
+  }
+
+  // get users
+  Future getMeals() async {
+    var client = BasicAuthClient('admin@gmail.com', 'admin');
+    var response = await client
+        .get(Uri.parse('https://javaops-demo.ru/topjava/rest/admin/users'));
+    var jsondata = jsonDecode(response.body);
+
+    for (var eachItem in jsondata) {
+      final user = User(
+        id: eachItem["id"],
+        name: eachItem["name"],
+        email: eachItem["email"],
+        isEnabled: eachItem["enabled"],
+      );
+      users.add(user);
+    }
+    //print(users.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getMeals();
+    return Scaffold(
+      // Background color of whole application
+      backgroundColor: Color(0xffe9ecef),
+      body: FutureBuilder(
+        future: getMeals(),
+        builder: (context, snapshot) {
+          //is it done loading?
+          if (snapshot.connectionState == ConnectionState.done) {
+            return BuilderForListview(users: users);
+          } else {
+            //still loading
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
+
+class BuilderForListview extends StatefulWidget {
+  const BuilderForListview({
+    super.key,
+    required this.users,
+  });
+
+  // Store all users here
+  final List<User> users;
+
+  @override
+  State<BuilderForListview> createState() => _BuilderForListviewState();
+}
+
+class _BuilderForListviewState extends State<BuilderForListview> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: widget.users.length,
+      itemBuilder: (context, index) {
+        // Variable for enabled status
+        bool isenabled = widget.users[index].isEnabled;
+        return Padding(
+          padding: const EdgeInsets.only(left: 2.0, top: 2.0, right:2.0),
+          child: Container(
+            decoration: BoxDecoration(
+              // Rounding corners of each record's box
+              borderRadius: BorderRadius.circular(20), 
+              // Background color of box
+              color: Color(0x3000d6c8)),
+            child: ListTile(
+              leading: CircleAvatar(
+                // Color depend of enabled status
+                backgroundColor: isenabled ? Color(0xff007bff) : Color(0x50007bff),
+                child: const Icon(
+                  // show icon
+                  Icons.person_outline_outlined,
+                  color: Color(0xffe9ecef),
+                ),
+              ),
+              textColor: isenabled ? Colors.black : Colors.grey,
+              title: Text(
+                //show user name
+                widget.users[index].name,
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+              subtitle: Text(
+                // show email
+                widget.users[index].email,
+                style: const TextStyle(fontWeight: FontWeight.w500),
+              ),
+              trailing: 
+              //Icon(Icons.keyboard_arrow_right),
+              Checkbox(
+                value: isenabled,
+                // when checkbox is pressed
+                onChanged: (value) {
+                  setState(() {
+                    widget.users[index].isEnabled = !widget.users[index].isEnabled;
+                  });
+                },
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
